3dd3664f39dcf9f1974c288fd8df69c8a5db3d93
Martin Cardenas <a1martinca11@gmail.com>
2023-10-09 09:06:18 -0700

    not using groupby and grouping directly from the backend

--- api-calls/getProjects.ts
+++ api-calls/getProjects.ts
@@ -1,6 +1,10 @@
 import paginatedFetcher from '@/utility/paginatedFetcher'
 import { type Project } from '@/interfaces/project'
 
-const getProjects = async (page: string, pageSize: string): Promise<{ data: Project | null, status: number }> => await paginatedFetcher('Project/all', page, pageSize)
+const getProjects = async (
+  page: string,
+  pageSize: string
+): Promise<{ data: Project[] | null, status: number }> =>
+  await paginatedFetcher('Project/all/groupedbycompany', page, pageSize)
 
 export default getProjects
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,7 +5,6 @@ import styles from './projectslist.module.css'
 import LoggedInCard from './LoggedInCard'
 import HeaderDescriptor from './HeaderDescriptor'
 import EachProject from './EachProject'
-import groupBy from '@/utility/groupBy'
 
 interface SearchParams {
   page: string
@@ -19,12 +18,8 @@ const ProjectsPage = async ({
   searchParams
 }: SearchParamsProps): Promise<JSX.Element> => {
   const { page } = searchParams
-  const data = await getProjects(page ?? '1', '5')
-  const projects = Array.isArray(data?.data) ? (data?.data as Project[]) : []
-
-  const result = groupBy({ items: projects }, (item) => item.company.name)
-
-  const resultArray = Object.entries(result)
+  const { data } = await getProjects(page ?? '1', '5')
+  const projects = data as Project[]
 
   return (
     <main className={styles.main}>
@@ -37,22 +32,21 @@ const ProjectsPage = async ({
           <LoggedInCard />
         </div>
         <HeaderDescriptor />
-        {Array.isArray(projects) && (
-          <ul>
-            {resultArray.map(([companyName, projects]) => (
-              <React.Fragment key={companyName}>
-                <div key={companyName} className={styles.companywrapper}>
-                  <h1>{companyName}</h1>
-                </div>
-                {projects?.map((project: Project) => (
+        <ul>
+          {Object.entries(projects).map(([companyName, projects]) => (
+            <React.Fragment key={companyName}>
+              <div key={companyName} className={styles.companywrapper}>
+                <h1>{companyName}</h1>
+              </div>
+              {Array.isArray(projects) &&
+                projects.map((project: Project) => (
                   <li key={project.projectId}>
                     <EachProject key={project.projectId} project={project} />
                   </li>
                 ))}
-              </React.Fragment>
-            ))}
-          </ul>
-        )}
+            </React.Fragment>
+          ))}
+        </ul>
       </section>
     </main>
   )
