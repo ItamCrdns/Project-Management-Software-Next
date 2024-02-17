ef3a0c4e047c032baf2d15902550ad562824d4a7
Martín Cárdenas <a1martinca11@gmail.com>
2024-02-17 04:58:05 -0700

    Removed unused Redux middleware

--- lib/store.ts
+++ lib/store.ts
@@ -1,15 +1,10 @@
-import { type Middleware, configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import { newProjectSlice } from './features/new project/newProjectSlice'
-
-const middleware: Middleware = (store) => (next) => (action) => {
-  next(action)
-}
 
 export const store = configureStore({
   reducer: {
     newProjectData: newProjectSlice.reducer
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middleware)
+  }
 })
 
 export type RootState = ReturnType<typeof store.getState>
