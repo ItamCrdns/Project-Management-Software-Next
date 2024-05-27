cc827aee8db4eab0b98619be8ddfdfaafd9707a4
Martin Cardenas <a1martinca11@outlook.com>
2024-05-26 20:45:56 -0700

    Use `Promise.all` to fetch both data concurrently in ClientInfoAndFilters component

--- app/clients/[clientId]/projects/(projects layout)/ClientInfoAndFilters.tsx
+++ app/clients/[clientId]/projects/(projects layout)/ClientInfoAndFilters.tsx
@@ -8,13 +8,15 @@ import { redirect } from 'next/navigation'
 const ClientInfoAndFilters: React.FC<{ clientId: string }> = async (props) => {
   const { clientId } = props
 
-  const { data, status } = await getCompany(clientId)
+  const companyData = getCompany(clientId)
+  const employeeTierData = getEmployeeTier()
 
-  if (data === null && status === 404) {
+  const [{ data: clientData, status: clientStatus }, { data: employeeTier }] =
+    await Promise.all([companyData, employeeTierData])
+
+  if (clientData === null && clientStatus === 404) {
     redirect('/')
   }
-
-  const { data: employeeTier } = await getEmployeeTier()
 
   return (
     <aside className='flex flex-col items-center gap-8 max-w-sm'>
@@ -24,12 +26,12 @@ const ClientInfoAndFilters: React.FC<{ clientId: string }> = async (props) => {
             className='font-bold text-theming-dark100 dark:text-theming-white100'
             href={`/clients/${clientId}`}
           >
-            {data?.name}
+            {clientData?.name}
           </Link>
           <div className='flex items-start gap-4'>
-            <p className='text-xs'>{data?.contactEmail}</p>
+            <p className='text-xs'>{clientData?.contactEmail}</p>
             <p className='select-none text-xs'>&middot;</p>
-            <p className='text-xs'>{data?.contactPhoneNumber}</p>
+            <p className='text-xs'>{clientData?.contactPhoneNumber}</p>
           </div>
         </div>
         <CreateNewProjectButton clientId={clientId} />
