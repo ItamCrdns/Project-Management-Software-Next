49e51aa3ee532dabfdbfa10a877837ac605f1bb9
Martín Cárdenas <a1martinca11@gmail.com>
2024-05-28 14:10:55 -0700

    Add go to last and first page arrows to ServerPagination component

--- components/pagination/ServerPagination.tsx
+++ components/pagination/ServerPagination.tsx
@@ -3,6 +3,8 @@ import { useGetSearchParams } from '../Filters/useGetSearchParams'
 import Previous from '../Advanced query params based pagination/arrows svgs/Previous'
 import Next from '../Advanced query params based pagination/arrows svgs/Next'
 import Link from 'next/link'
+import First from '../Advanced query params based pagination/arrows svgs/First'
+import Last from '../Advanced query params based pagination/arrows svgs/Last'
 
 const ServerPagination = ({
   totalPages,
@@ -17,6 +19,15 @@ const ServerPagination = ({
     <div className='flex items-center justify-center gap-4 p-4'>
       <Link
         href={(() => {
+          searchParams.set('page', '1')
+
+          return `${pathname}?${searchParams.toString()}`
+        })()}
+      >
+        <First />
+      </Link>
+      <Link
+        href={(() => {
           if (currentPage > 1) {
             searchParams.set('page', (currentPage - 1).toString())
           }
@@ -26,7 +37,7 @@ const ServerPagination = ({
       >
         <Previous />
       </Link>
-      <p>
+      <p className='text-xs text-gray-400'>
         {currentPage} of {totalPages}
       </p>
       <Link
@@ -39,6 +50,15 @@ const ServerPagination = ({
         })()}
       >
         <Next />
+      </Link>
+      <Link
+        href={(() => {
+          searchParams.set('page', totalPages.toString())
+
+          return `${pathname}?${searchParams.toString()}`
+        })()}
+      >
+        <Last />
       </Link>
     </div>
   )
